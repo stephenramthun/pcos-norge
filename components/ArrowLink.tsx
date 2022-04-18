f56b3c7022132af77760d8bed9bd4b11bddb7100
Stephen Ramthun <stephenramthun@gmail.com>
2022-04-18 11:10:54 +0200

    Simplify component api

--- components/ArrowLink.tsx
+++ components/ArrowLink.tsx
@@ -1,41 +1,20 @@
 import React from "react"
 import classNames from "classnames"
-import { ArrowDown, ArrowLeft, ArrowRight, ArrowUp } from "phosphor-react"
+import { ArrowRight } from "phosphor-react"
 
 import styles from "./ArrowLink.module.css"
 
-type Direction = "up" | "right" | "down" | "left"
-
 interface ArrowLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
   href: string
-  arrow?: Direction
 }
 
 export const ArrowLink: React.FC<ArrowLinkProps> = ({
   className,
   children,
-  arrow = "right",
   ...linkProps
 }) => (
-  <a
-    className={classNames(styles.Link, className, arrow && styles[arrow])}
-    {...linkProps}
-  >
+  <a className={classNames(styles.Link, className)} {...linkProps}>
     {children}
-    {(() => {
-      const size = 24
-      switch (arrow) {
-        case "up":
-          return <ArrowUp weight="bold" size={size} />
-        case "right":
-          return <ArrowRight weight="bold" size={size} />
-        case "down":
-          return <ArrowDown weight="bold" size={size} />
-        case "left":
-          return <ArrowLeft weight="bold" size={size} />
-        default:
-          return null
-      }
-    })()}
+    <ArrowRight weight="bold" size={24} />
   </a>
 )
