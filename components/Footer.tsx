b6b498dd9b9d06e6f88a6b8f8dff0911584c2cfa
Stephen Ramthun <stephenramthun@gmail.com>
2023-01-25 19:29:51 +0100

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,38 +1,66 @@
 import React from "react"
 
-import styles from "./Footer.module.css"
-
 import { Logo } from "./Logo"
 import { Body } from "./Body"
 import { Content } from "./Content"
-import { Link } from "@components/Link"
+import { Link } from "components/Link"
+
+import styles from "./Footer.module.css"
 
 export const Footer: React.FC = () => (
   <footer className={styles.container}>
     <Content className={styles.content}>
-      <div className={styles.details}>
-        <div>
-          <Logo variant="light" className={styles.logo} priority={false} />
-          <span>
+      <Logo variant="light" priority={false} />
+      <div>
+        <article className={styles.details}>
+          <div className={styles.grid}>
             <Body>Epost:</Body>
-            <Body>post@pcosnorge.no</Body>
-          </span>
-          <span>
+            <Link href="mailto:post@pcosnorge.no">post@pcosnorge.no</Link>
+            <Body>Adresse:</Body>
+            <div className={styles.address}>
+              <Body>c/o Regine Vinnes Wiig Andersen</Body>
+              <Body>Mjåtveitstø 32</Body>
+              <Body>5918 FREKHAUG</Body>
+            </div>
             <Body>Org.nr:</Body>
             <Body>927 818 906</Body>
-          </span>
-          <span>
-            <Link href="/personvernerklæring">Personvernerklæring</Link>
-          </span>
-        </div>
-        <div className={styles.description}>
-          <Body>
+          </div>
+          <Link className={styles.privacy} href="/personvernerklæring">
+            Les personvernserklæringen vår
+          </Link>
+        </article>
+        <article>
+          <nav className={styles.nav}>
+            <ul>
+              <li>
+                <Link href="/hva-er-pcos">Hva er PCOS</Link>
+              </li>
+              <li>
+                <Link href="/om-oss">Om oss</Link>
+              </li>
+              <li>
+                <Link href="/aktuelt">Aktuelt</Link>
+              </li>
+              <li>
+                <Link href="/bidra">Bidra</Link>
+              </li>
+              <li>
+                <Link href="/min-side">Min side</Link>
+              </li>
+              <li>
+                <Link href="/bli-medlem">Bli medlem</Link>
+              </li>
+            </ul>
+          </nav>
+        </article>
+        <article>
+          <Body className={styles.description}>
             PCOS Norge skal jobbe for mer synlighet, normalisering og
             oppmerksomhet rundt diagnosen blant folk flest og i media. Vi skal
             bidra til å enkelt tilgjengeliggjøre oppdatert og forskningsbasert
             informasjon om syndromet og sørge for bedre kunnskap i helsevesenet.
           </Body>
-        </div>
+        </article>
       </div>
     </Content>
   </footer>
