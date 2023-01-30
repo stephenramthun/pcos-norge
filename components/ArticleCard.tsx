2a355c62f260258b6c79b43886bc354423a2a4d6
Stephen Ramthun <stephenramthun@gmail.com>
2023-01-30 09:31:29 +0100

    Remove unneeded elements

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -4,12 +4,12 @@ import classNames from "classnames"
 import { Body } from "@components/Body"
 import { Heading } from "@components/Heading"
 import { ArrowLink } from "@components/ArrowLink"
+import { SanityImage } from "@components/SanityImage"
 import { useLocaleDateString } from "@hooks/useLocaleDateString"
 
 import { ImageAsset } from "types/schema"
 
 import styles from "./ArticleCard.module.css"
-import { SanityImage } from "@components/SanityImage"
 
 interface ArticleCardProps extends React.HTMLAttributes<HTMLDivElement> {
   slug: string
@@ -26,31 +26,33 @@ export const ArticleCard: React.VFC<ArticleCardProps> = ({
   published,
   ingress,
   className,
-  ...divProps
+  ...elementProps
 }) => {
   const date = useLocaleDateString(published)
 
   return (
-    <div {...divProps} className={classNames(styles.container, className)}>
-      <article key={slug} className={styles.article}>
-        <div className={styles.imageContainer}>
-          <SanityImage alt={image.alt ?? ""} asset={image.asset} />
-        </div>
-        <Body suppressHydrationWarning className={styles.date}>
-          {date}
-        </Body>
-        <Heading tag="h3" size="small">
-          {title}
-        </Heading>
-        <Body>{ingress}</Body>
-        <ArrowLink href={`/aktuelt/${slug}`}>Les mer</ArrowLink>
-        <a
-          href={`/aktuelt/${slug}`}
-          className={styles.cardLink}
-          aria-label={slug}
-          tabIndex={-1}
-        />
-      </article>
-    </div>
+    <article
+      key={slug}
+      className={classNames(styles.article, className)}
+      {...elementProps}
+    >
+      <div className={styles.imageContainer}>
+        <SanityImage alt={image.alt ?? ""} asset={image.asset} />
+      </div>
+      <Body suppressHydrationWarning className={styles.date}>
+        {date}
+      </Body>
+      <Heading tag="h3" size="small">
+        {title}
+      </Heading>
+      <Body>{ingress}</Body>
+      <ArrowLink href={`/aktuelt/${slug}`}>Les mer</ArrowLink>
+      <a
+        href={`/aktuelt/${slug}`}
+        className={styles.cardLink}
+        aria-label={slug}
+        tabIndex={-1}
+      />
+    </article>
   )
 }
