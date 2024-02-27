296c86107ee1514e927f07408b7b2c0873a40154
Stephen Ramthun <stephenramthun@gmail.com>
2024-02-27 14:32:32 +0100

    Legg til "Hva er PCOS?"-seksjon på forsiden

---
+++ apps/site/pages/HvaErPcosCard.tsx
@@ -0,0 +1,44 @@
+import { ArrowRight } from "@phosphor-icons/react"
+import { ImageAsset } from "@sanity/types"
+import React from "react"
+
+import { SanityImageDocument } from "../types/sanity"
+import { Body } from "components/Body"
+import { Heading } from "components/Heading"
+import { LinkButton } from "components/LinkButton"
+import { SanityImage } from "components/SanityImage"
+
+import styles from "./HvaErPcosCard.module.css"
+
+type Image = Omit<SanityImageDocument, "imageAsset"> & {
+  asset: ImageAsset
+  alt: string
+}
+
+type Props = {
+  image: Image
+}
+
+export const HvaErPcosCard: React.FC<Props> = ({ image }) => (
+  <article className={styles.hvaErPcos}>
+    <section className={styles.textContainer}>
+      <Heading tag="h2" size="medium">
+        Hva er PCOS?
+      </Heading>
+      <Body>
+        Polycystisk ovariesyndrom (PCOS) er den vanligste hormonelle (endokrine)
+        forstyrrelsen hos kvinner. Tilstanden rammer 10-15% av alle kvinner og
+        kan gi mange helseproblemer som følge av den hormonelle ubalansen. Alle
+        med PCOS opplever diagnosen forskjellig, men det finnes noen fellestrekk
+        i symptombildet.
+      </Body>
+      <LinkButton href="/hva-er-pcos">
+        Les mer om diagnosen
+        <ArrowRight size={24} />
+      </LinkButton>
+    </section>
+    <div className={styles.imageContainer}>
+      <SanityImage asset={image.asset} alt={image.alt} maxWidth={1000} fill />
+    </div>
+  </article>
+)
--- apps/site/pages/index.page.tsx
+++ apps/site/pages/index.page.tsx
@@ -1,3 +1,4 @@
+import { ImageAsset } from "@sanity/types"
 import type { GetStaticProps, GetStaticPropsResult, NextPage } from "next"
 import React from "react"
 
@@ -9,17 +10,27 @@ import { Hero } from "components/Hero"
 import { Main } from "components/Main"
 import { PageContainer } from "components/PageContainer"
 import { getClient } from "io/sanity/client"
-import { ArticleObject } from "types/sanity"
+import { ArticleObject, SanityImageDocument } from "types/sanity"
 
 import { Articles } from "./Articles"
+import { HvaErPcosCard } from "./HvaErPcosCard"
 
 import styles from "./index.module.css"
 
-interface HomeProps {
-  articles: Array<Omit<ArticleObject, "body">>
+type Image = Omit<SanityImageDocument, "imageAsset"> & {
+  asset: ImageAsset
+  alt: string
 }
 
-const Home: NextPage<HomeProps> = ({ articles }) => {
+interface HomeProps {
+  articles: Array<Omit<ArticleObject, "body">>
+  images: {
+    hvaErPcos: Image
+  }
+}
+
+const Home: NextPage<HomeProps> = ({ articles, images }) => {
+  console.log(images)
   return (
     <PageContainer>
       <Head />
@@ -28,6 +39,9 @@ const Home: NextPage<HomeProps> = ({ articles }) => {
       <Main id="main">
         <Content className={styles.section}>
           <Articles articles={articles} />
+        </Content>
+        <Content className={styles.section}>
+          <HvaErPcosCard image={images.hvaErPcos} />
         </Content>
       </Main>
       <Footer />
@@ -50,6 +64,14 @@ export const getStaticProps: GetStaticProps = async (): Promise<
         },
         published,
         ingress
+      },
+      "images": {
+        "hvaErPcos": *[_type == "imageDocument" && id.current == "hva-er-pcos"][0] {
+          title,
+          "asset": imageAsset.asset->,
+          "alt": imageAsset.alt,
+          id
+        }
       },
     }
   `)
